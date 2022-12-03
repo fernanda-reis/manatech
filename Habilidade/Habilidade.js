a43810d23d8acf8f447d1650c785ce0fd568b98c
Fernanda Soares <fernanda.soares@elogroup.com.br>
2022-12-03 13:00:06 -0300

    add classe Habilidade

---
+++ Habilidade/Habilidade.js
@@ -0,0 +1,14 @@
+class Habilidade {
+  id;
+  descricao;
+  tecnologias = [];
+
+  constructor(descricao) {
+    this.id = id;
+    this.descricao = descricao;
+  }
+
+  cadastrarHabilidade() {}
+}
+
+module.exports = Habilidade;
